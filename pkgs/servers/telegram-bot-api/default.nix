b5f3e0de51490db3bb08618212a8e3a91ea24500
Forden <24463229+Forden@users.noreply.github.com>
2024-05-17 04:48:09 +0300

    telegram-bot-api: 5.7 -> 7.3

--- pkgs/servers/telegram-bot-api/default.nix
+++ pkgs/servers/telegram-bot-api/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation {
   pname = "telegram-bot-api";
-  version = "5.7";
+  version = "7.3";
 
   src = fetchFromGitHub {
     repo = "telegram-bot-api";
     owner = "tdlib";
     rev = "c57b04c4c8c4e8d8bb6fdd0bd3bfb5b93b9d8f05";
-    sha256 = "sha256-WetzX8GBdwQAnnZjek+W4v+QN1aUFdlvs+Jv6n1B+eY=";
+    sha256 = "";
     fetchSubmodules = true;
   };
 
@@ -19,7 +19,7 @@ stdenv.mkDerivation {
     description = "Telegram Bot API server";
     homepage = "https://github.com/tdlib/telegram-bot-api";
     license = licenses.boost;
-    maintainers = with maintainers; [ Anillc ];
+    maintainers = with maintainers; [ Anillc forden ];
     platforms = platforms.all;
     mainProgram = "telegram-bot-api";
   };
